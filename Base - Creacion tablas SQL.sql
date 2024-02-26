b432e297dcaeb42a11c56d3b356c1c23ebd112e8
Beatriz <b.sanchez.reguera6@gmail.com>
2024-02-26 13:16:00 +0100

    actualizacion

--- Base - Creacion tablas SQL.sql
+++ Base - Creacion tablas SQL.sql
@@ -1,49 +1,43 @@
 -- Proyecto -Equipo 3 - Tablas SQL
 
--- Uso de tabla:
-CREATE SCHEMA ;
-USE 
 
 -- Tabla Peliculas -- 
 CREATE TABLE Peliculas (
-	id_pelicula INT AUTO_INCREMENT PRIMARY KEY,
+	id_pelicula VARCHAR(10) PRIMARY KEY,
+    genero VARCHAR(10),
     tipo VARCHAR(10),
     nombre_pelicula VARCHAR(255),
     año_estreno YEAR,
-    mes_estreno TINYINT;
-    -- id_genero INT ??
-    -- FOREIGN KEY (id_genero) REFERENCES generos(id_genero)??
-);
+    mes_estreno TINYINT
+ );
 
 -- Tabla Detalles Peliculas -- 
 CREATE TABLE Detalles_Peliculas (
-	id_detalle INT AUTO_INCREMENT PRIMARY KEY,
-    id_pelicula INT,
-    puntuacion_IMDB FLOAT,
-    puntuacion_Rotten_Tomatoes FLOAT,
-    director VARCHAR(255) O TEXT,
-    guionistas VARCHAR(255) O TEXT,
+	id_pelicula VARCHAR(10),
+    puntuacion_IMDB VARCHAR(10),
+    puntuacion_Rotten_Tomatoes VARCHAR(10),
+    director TEXT,
+    guionistas TEXT,
     argumento TEXT, 
     duracion_minutos TIME, 
-    -- FOREIGN KEY (id_PELICULA) REFERENCES Peliculas(id_pelicula)??
+    CONSTRAINT `fk_id_pelicula`
+    FOREIGN KEY (id_PELICULA) REFERENCES Peliculas(id_pelicula)
 );
 
 -- Tabla Actores -- 
 CREATE TABLE Actores (
-	id_actor INT AUTO_INCREMENT PRIMARY KEY,
-    id_pelicula PRIMARY KEY O SOLO INT, -- ??
+	id_pelicula VARCHAR(10),
     nombre_actor VARCHAR(255),
     año_nacimiento YEAR,
-    conocido_por TEXT,
-    ocupacion TEXT,
+    titulos TEXT,
+    rol TEXT,
     premios TEXT,
+    CONSTRAINT `fk_id_pelicula`
+    FOREIGN KEY (id_PELICULA) REFERENCES Peliculas(id_pelicula)
 );
 
--- Tabla Relacion Pelicula -- 
-CREATE TABLE Relacion_Pelicula (
-	id_relacion INT AUTO_INCREMENT PRIMARY KEY,
-    id_pelicula INT,
-    id_genero INT,
-    -- FOREIGN KEY (id_pelicula) REFERENCES Peliculas(id_pelicula)??
-    -- FOREIGN KEY (id_genero) REFERENCES generos(id_genero)??
-);
+LOAD DATA INFILE 'proyecto_json_generos/accion.csv' 
+INTO TABLE peliculas
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; -- Ignora la primera fila si es una fila de encabezado
