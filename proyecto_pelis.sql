3a2a219941c2bfcf694ec28406fc1a9c8e48811a
macresca <mariacrespocalabuig@gmail.com>
2024-03-09 11:16:22 +0100

    update sql

--- proyecto_pelis.sql
+++ proyecto_pelis.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS Actores (
     Año_nacimiento VARCHAR(10),
     Profesion VARCHAR(255),
     Conocido_por VARCHAR(255),
-    PRIMARY KEY (id_actor),
+    PRIMARY KEY (id_actor)
 );
 
 USE proyecto_pelis;
@@ -57,8 +57,8 @@ CREATE TABLE IF NOT EXISTS Premios (
     Mejor_actriz VARCHAR (255),
     id_actriz VARCHAR (100) NOT NULL,
     PRIMARY KEY (id_premio),
-    FOREIGN KEY (id_actor) REFERENCES Actor(id_actor),
-    FOREIGN KEY (id_actriz) REFERENCES Actor(id_actor),
+    FOREIGN KEY (id_actor) REFERENCES Actores(id_actor),
+    FOREIGN KEY (id_actriz) REFERENCES Actores(id_actor),
     FOREIGN KEY (id_pelicula) REFERENCES Peliculas(id_pelicula)
 );
 
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS Peli_actor (
 id_pelicula VARCHAR (10) NOT NULL,
 FOREIGN KEY (id_pelicula) REFERENCES Detalles_Peliculas(id_pelicula),
 id_actor VARCHAR (100) NOT NULL,
-FOREIGN KEY (id_actor) REFERENCES Actores(id_actor),
+FOREIGN KEY (id_actor) REFERENCES Actores(id_actor)
 );
 
 CREATE TABLE IF NOT EXISTS actor_premio (
@@ -77,11 +77,3 @@ FOREIGN KEY (id_actor) REFERENCES Actores(id_actor),
 id_premio INT NOT NULL AUTO_INCREMENT,
 FOREIGN KEY (id_premio) REFERENCES Premios(id_premio)
 );
-
-
-
-LOAD DATA INFILE 'proyecto_json_generos/accion.csv' 
-INTO TABLE peliculas
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS; -- Ignora la primera fila si es una fila de encabezado
